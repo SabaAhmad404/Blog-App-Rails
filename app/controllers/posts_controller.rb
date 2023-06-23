021caa901b31304b4f5ce10d887d8849cd846949
SabaAhmad404 <sabaahmad895@gmail.com>
2023-06-23 12:51:33 +0500

    Fix linters errors

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,17 +1,16 @@
 class PostsController < ApplicationController
   def index
-    @posts=Post.where(author_id: params[:user_id])
-    @users=User.find(params[:user_id])
+    @posts = Post.where(author_id: params[:user_id])
+    @users = User.find(params[:user_id])
   end
 
   def new
-  @post_new=Post.new
+    @post_new = Post.new
   end
 
   def show
-    @post=Post.find(params[:id])
-    @comments=@post.comments
-    @user=User.find(params[:id])
-
+    @post = Post.find(params[:id])
+    @comments = @post.comments
+    @user = User.find(params[:id])
   end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,13 +1,13 @@
 class UsersController < ApplicationController
   def index
-    @users=User.all
+    @users = User.all
   end
 
   def new
-   @users_new=User.new
+    @users_new = User.new
   end
 
   def show
-    @user=User.find(params[:id])
+    @user = User.find(params[:id])
   end
 end
